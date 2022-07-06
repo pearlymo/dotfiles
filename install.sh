e085c2ac75f0b49068f0878b33c9964afda0e08b
pearlymo <mothi.ghimire@gmail.com>
2022-07-05 22:19:39 -0600

    installer: Remove bad whitespaces and unnecessary comments

--- install.sh
+++ install.sh
@@ -1,54 +1,44 @@
 #!/bin/bash
 
-########    
-# nvim #    
-########    
-    
-mkdir -p "$XDG_CONFIG_HOME/nvim"    
+# nvim
+
+mkdir -p "$XDG_CONFIG_HOME/nvim"
 mkdir -p "$XDG_CONFIG_HOME/nvim/undo"
 
 ln -sf "$DOTFILES/nvim/init.vim" "$XDG_CONFIG_HOME/nvim"
 ln -sf "$DOTFILES/nvim/templates" "$XDG_CONFIG_HOME/nvim"
 ln -sf "$DOTFILES/nvim/colors" "$XDG_CONFIG_HOME/nvim"
 
-rm -rf "$XDG_CONFIG_HOME/X11"    
+# X11
+
+rm -rf "$XDG_CONFIG_HOME/X11"
 ln -s "$DOTFILES/X11" "$XDG_CONFIG_HOME"
 
-######    
-# i3 #    
-######    
-    
-rm -rf "$XDG_CONFIG_HOME/i3"    
+# i3
+
+rm -rf "$XDG_CONFIG_HOME/i3"
 ln -s "$DOTFILES/i3" "$XDG_CONFIG_HOME"
 
-#######    
-# Zsh #    
-#######    
-    
-mkdir -p "$XDG_CONFIG_HOME/zsh"    
-ln -sf "$DOTFILES/zsh/.zshenv" "$HOME"    
+# Zsh
+
+mkdir -p "$XDG_CONFIG_HOME/zsh"
+ln -sf "$DOTFILES/zsh/.zshenv" "$HOME"
 ln -sf "$DOTFILES/zsh/.zshrc" "$XDG_CONFIG_HOME/zsh"
 ln -sf "$DOTFILES/zsh/aliases" "$XDG_CONFIG_HOME/zsh/aliases"
-rm -rf "$XDG_CONFIG_HOME/zsh/external"    
+rm -rf "$XDG_CONFIG_HOME/zsh/external"
 ln -sf "$DOTFILES/zsh/external" "$XDG_CONFIG_HOME/zsh"
 
-#########    
-# Fonts #    
-#########    
-    
-mkdir -p "$XDG_DATA_HOME"    
+# Fonts
+
+mkdir -p "$XDG_DATA_HOME"
 cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME"
 
-#########    
-# dunst #    
-#########    
-    
-mkdir -p "$XDG_CONFIG_HOME/dunst"    
+# dunst
+
+mkdir -p "$XDG_CONFIG_HOME/dunst"
 ln -sf "$DOTFILES/dunst/dunstrc" "$XDG_CONFIG_HOME/dunst/dunstrc"
 
-########
-# tmux #
-########
+# tmux
 
 mkdir -p "$XDG_CONFIG_HOME/tmux"
 ln -sf "$DOTFILES/tmux/tmux.conf" "$XDG_CONFIG_HOME/tmux/tmux.conf"
