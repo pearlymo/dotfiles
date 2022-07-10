65d03e3f76386ed3b7c2941ebefdbc1253ad8494
pearlymo <mothi.ghimire@gmail.com>
2022-07-10 01:57:19 -0600

    zsh: Remove functions for ddg and wikipedia using lynx

--- zsh/scripts.sh
+++ zsh/scripts.sh
@@ -1,16 +1,7 @@
 #!/bin/zsh
 
-# Compress a directory
 compress() {
     tar cvzf $1.tar.gz $1
-}
-
-duckduckgo() {
-    lynx -vikeys -accept_all_cookies "https://lite.duckduckgo.com/lite/?q=$@"
-}
-
-wikipedia() {
-    lynx -vikeys -accept_all_cookies "https://en.wikipedia.org/wiki?search=$@"
 }
 
 gacp () {
